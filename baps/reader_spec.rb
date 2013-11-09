354287be708094f96e445862cae0f3344a641086
mattbw <matt.windsor@ury.york.ac.uk>
2013-11-09 02:38:09 +0000

    Improve RSpec a bit.

--- baps/reader_spec.rb
+++ baps/reader_spec.rb
@@ -12,30 +12,28 @@ UINT32_MAX = 4_294_967_295
 UINT32S = [UINT32_MIN, UINT32_MID, UINT32_MAX]
 
 describe Bra::Baps::Reader do
+  before :each do
+    @reader = Bra::Baps::Reader.new
+  end
+
   describe '#uint16' do
     context 'with no data' do
       it 'returns nil' do
-        reader = Bra::Baps::Reader.new
-
-        reader.uint16.should be_nil
+        @reader.uint16.should be_nil
       end
     end
     context 'with one datum' do
       it 'reads an unsigned 16-bit integer' do
-        reader = Bra::Baps::Reader.new
-
         UINT16S.each do |n|
-          reader.add([n].pack('n'))
-          reader.uint16.should eq(n)
+          @reader.add([n].pack('n'))
+          @reader.uint16.should eq(n)
         end
       end
     end
     context 'with multiple data' do
       it 'reads unsigned 16-bit integers in order' do
-        reader = Bra::Baps::Reader.new
-
-        UINT16S.each { |n| reader.add([n].pack('n')) }
-        UINT16S.each { |n| reader.uint16.should eq(n) }
+        UINT16S.each { |n| @reader.add([n].pack('n')) }
+        UINT16S.each { |n| @reader.uint16.should eq(n) }
       end
     end
   end
@@ -43,27 +41,21 @@ describe Bra::Baps::Reader do
   describe '#uint32' do
     context 'with no data' do
       it 'returns nil' do
-        reader = Bra::Baps::Reader.new
-
-        reader.uint16.should be_nil
+        @reader.uint16.should be_nil
       end
     end
     context 'with one datum' do
       it 'reads an unsigned 32-bit integer' do
-        reader = Bra::Baps::Reader.new
-
         UINT32S.each do |n|
-          reader.add([n].pack('N'))
-          reader.uint32.should eq(n)
+          @reader.add([n].pack('N'))
+          @reader.uint32.should eq(n)
         end
       end
     end
     context 'with multiple data' do
       it 'reads unsigned 32-bit integers in order' do
-        reader = Bra::Baps::Reader.new
-
-        UINT32S.each { |n| reader.add([n].pack('N')) }
-        UINT32S.each { |n| reader.uint32.should eq(n) }
+        UINT32S.each { |n| @reader.add([n].pack('N')) }
+        UINT32S.each { |n| @reader.uint32.should eq(n) }
       end
     end
   end
@@ -71,42 +63,34 @@ describe Bra::Baps::Reader do
   describe '#raw_bytes' do
     context 'with no data' do
       it 'returns nil' do
-        reader = Bra::Baps::Reader.new
-
-        reader.raw_bytes(10).should be_nil
+        @reader.raw_bytes(10).should be_nil
       end
     end
     context 'with insufficient data' do
       it 'returns nil' do
-        reader = Bra::Baps::Reader.new
-
-        reader.add('The quick brown fox jumps over the lazy dog.')
-        reader.raw_bytes(200).should be_nil
+        @reader.add('The quick brown fox jumps over the lazy dog.')
+        @reader.raw_bytes(200).should be_nil
       end
     end
     context 'with sufficient data' do
       it 'reads the number of bytes requested' do
-        reader = Bra::Baps::Reader.new
-
         str = 'The quick brown fox jumps over the lazy dog.'
-        reader.add(str)
-        reader.raw_bytes(str.bytesize).should eq(str)
-        reader.raw_bytes(1).should be_nil
+        @reader.add(str)
+        @reader.raw_bytes(str.bytesize).should eq(str)
+        @reader.raw_bytes(1).should be_nil
       end
     end
     context 'with more than enough data' do
       it 'reads the number of bytes requested' do
-        reader = Bra::Baps::Reader.new
-
-        reader.add('The quick brown fox jumps over the lazy dog.')
-        reader.raw_bytes(10).should eq('The quick ')
-        reader.raw_bytes(10).should eq('brown fox ')
-        reader.raw_bytes(10).should eq('jumps over')
-        reader.raw_bytes(10).should eq(' the lazy ')
-        reader.raw_bytes(2).should eq('do')
-        reader.raw_bytes(1).should eq('g')
-        reader.raw_bytes(1).should eq('.')
-        reader.raw_bytes(1).should be_nil
+        @reader.add('The quick brown fox jumps over the lazy dog.')
+        @reader.raw_bytes(10).should eq('The quick ')
+        @reader.raw_bytes(10).should eq('brown fox ')
+        @reader.raw_bytes(10).should eq('jumps over')
+        @reader.raw_bytes(10).should eq(' the lazy ')
+        @reader.raw_bytes(2).should eq('do')
+        @reader.raw_bytes(1).should eq('g')
+        @reader.raw_bytes(1).should eq('.')
+        @reader.raw_bytes(1).should be_nil
       end
     end
   end
