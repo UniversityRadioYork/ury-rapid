f7f043a7ce035dfaafb90ec32d1a8c8993b9fcae
mattbw <matt.windsor@ury.york.ac.uk>
2013-11-17 22:15:28 +0000

    Widen targets for playlist handler.

--- baps/requests/handlers/playlist.rb
+++ baps/requests/handlers/playlist.rb
@@ -4,21 +4,44 @@ module Bra
   module Baps
     module Requests
       module Handlers
-        # Handler for playlist
+        # Handler for playlists
+        #
+        # This handler also targets channels and channel sets, because these
+        # all have similar DELETE semantics.
         class Playlist < Handler
           # The handler targets matched by this handler.
-          TARGETS = [:playlist]
+          TARGETS = [:playlist, :channel, :channel_set]
 
           # Requests a playlist be DELETEd via the BAPS server
           # 
           # This resets the playlist.
           def delete(object)
-            send(Request.new(Codes::Playlist::RESET, object.channel_id))
+            case object.handler_target 
+            when :playlist
+              reset(object.channel_id)
+            when :channel
+              reset(object.id)
+            when :channel_set
+              object.children.map(&:id).each(&method(:reset))
+            end
 
             false
           end
 
           # TODO(mattbw): PUT
+
+          private
+
+          # Resets a playlist given its channel ID.
+          #
+          # @api private
+          #
+          # @param id [Integer] The ID of the channel to reset.
+          #
+          # @return [void]
+          def reset(id)
+            send(Request.new(Codes::Playlist::RESET, id))
+          end
         end
       end
     end
