d22737509166cda6730b273b21b0578e50d86d3f
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-11 23:22:02 +0000

    Tidy up server and main code.
    The server app is now in a new Server module, alongside some launcher code
    factored out of the main code.

--- main.rb
+++ main.rb
@@ -2,118 +2,77 @@ require 'eventmachine'
 require 'thin'
 require 'yaml'
 require 'active_support/core_ext/hash/keys'
-require_relative 'server_app'
+require_relative 'server/launcher'
 require_relative 'models/creator'
 require_relative 'common/config_authenticator'
 
-# Internal: Creates the dispatch for the reactor.
-#
-# web_app - The app to map to /.
-#
-# Returns the dispatch.
-def make_dispatch(config, web_app)
-  Rack::Builder.app do
-    map(config[:root]) { run(web_app) }
-  end
-end
-
-# Internal: Starts the server.
-#
-# dispatch - The dispatch to use for the server.
-# server   - The name of the server to start.
-# host     - The hostname of the server.
-# port     - The port on which the server will be started.
-#
-# @return [void]
-def start_server(dispatch, server, host, port)
-  Rack::Server.start({ app: dispatch, server: server, Host: host, Port: port })
-end
-
-# Makes sure the server supplied can run EventMachine
-#
-# server - The name of the server to check.
-#
-# @return [void]
-# Raises a string error if the server does not appear to be EM compatible.
-def check_server_em_compatible(server)
-  fail("Need an EM server, but #{server} isn't") unless em_compatible?(server)
-end
-
-# Decides whether the server supplied can run EventMachine
-#
-# server - The name of the server to check.
-#
-# Returns true if the server is compatible, and false otherwise.
-def em_compatible?(server)
-  %w(thin hatetepe goliath).include?(server)
-end
-
-# Creates the authenticator for the server
-def authenticator(config)
-  Bra::Common::ConfigAuthenticator.new(config[:server][:users])
-end
+CONFIG_FILE = 'config.yml'
 
 # Runs bra (this is the main function)
 def run
-  config = YAML.load_file('config.yml').deep_symbolize_keys!
+  config = load_config
+  app = App.new(config)
+  EventMachine.run { app.run }
+end
 
-  driver = init_driver(config[:driver])
-  model = init_model(config[:model], driver)
-  app = Bra::ServerApp.new(config[:server], model, authenticator(config))
+# Loads the bra configuration.
+def load_config
+  YAML.load_file(CONFIG_FILE).deep_symbolize_keys!
+end
 
-  EventMachine.run do
-    setup_server(config[:server], app)
-    driver.run(model)
+class App
+  def initialize(config)
+    @driver = init_driver(config[:driver])
+    @model = init_model(config[:model], @driver)
+    @server = init_server(config[:server], @model)
   end
-end
 
-# Find the correct driver from the config, and initialise it
-#
-# The driver is the part of the bra system that interfaces with the playout
-# system.  It runs in bra's EventMachine instance and is wired into the bra
-# model.
-def init_driver(config)
-  driver_module = config[:source]
-  require driver_module
+  def run
+    @server.run
+    @driver.run(@model)
+  end
 
-  Driver.new(config)
-end
+  private
 
-# Create a model from its config and the playout system driver.
-def init_model(init_config, driver)
-  # Make an updates channel here, because it's neither the driver's
-  # responsibility, nor can it easily be made in the YAML.
-  init_config[:update_channel] = EventMachine::Channel.new
+  # Find the correct driver from the config, and initialise it
+  #
+  # The driver is the part of the bra system that interfaces with the playout
+  # system.  It runs in bra's EventMachine instance and is wired into the bra
+  # model.
+  def init_driver(config)
+    driver_module = config[:source]
+    require driver_module
 
-  # The model config is created by loading up driver-neutral configuration from
-  # the central YAML file, then passing it to the driver to check for
-  # unsound config (like requesting 3 channels on a single-channel playout) and
-  # to add in driver-specific configuration, like model handlers.
-  full_config = driver.configure_model(init_config)
+    Driver.new(config)
+  end
 
-  # Now we create the driver-neutral model, but the driver might want to add
-  # its own model items into the model space, so we pass the model back to the
-  # driver to post-process.
-  creator = Bra::Models::Creator.new(full_config)
-  driver.process_model(creator.create)
-end
+  def init_server(config, model)
+    Bra::Server::Launcher.new(config, model, authenticator(config))
+  end
 
-# Initialises the server end of bra
-#
-# This handles requests to and responses from the environment.
-#
-# @param config [Hash] The bra server configuration.
-# @param app [ServerApp] The server application.
-#
-# @return [void]
-def setup_server(config, app)
-  server, host, port = config.values_at(*%i(rack host port))
+  # Create a model from its config and the playout system driver.
+  def init_model(init_config, driver)
+    # Make an updates channel here, because it's neither the driver's
+    # responsibility, nor can it easily be made in the YAML.
+    init_config[:update_channel] = EventMachine::Channel.new
 
-  dispatch = make_dispatch(config, app)
+    # The model config is created by loading up driver-neutral configuration
+    # from the central YAML file, then passing it to the driver to check for
+    # unsound config (like requesting 3 channels on a single-channel playout)
+    # and to add in driver-specific configuration, like model handlers.
+    full_config = driver.configure_model(init_config)
 
-  check_server_em_compatible(server)
+    # Now we create the driver-neutral model, but the driver might want to add
+    # its own model items into the model space, so we pass the model back to
+    # the driver to post-process.
+    creator = Bra::Models::Creator.new(full_config)
+    driver.process_model(creator.create)
+  end
 
-  start_server(dispatch, server, host, port)
+  # Creates the authenticator for the server
+  def authenticator(config)
+    Bra::Common::ConfigAuthenticator.new(config[:users])
+  end
 end
 
 run if __FILE__ == $PROGRAM_NAME
---
+++ server/launcher.rb
@@ -0,0 +1,60 @@
+require 'rack'
+require_relative 'app'
+
+module Bra
+  module Server
+    # Object for launching the bra server.
+    class Launcher
+      def initialize(config, model, authenticator)
+        @app = App.new(config, model, authenticator)
+        @rack, @host, @port, @root = config.values_at(*%i{rack host port root})
+        @dispatch = make_dispatch
+
+        check_server_em_compatible
+      end
+
+      # Starts the server
+      #
+      # This should be called within an EventMachine instance.
+      #
+      # @return [void]
+      def run
+        Rack::Server.start(
+          app: @dispatch, server: @rack, Host: @host, Port: @port
+        )
+      end
+
+      private
+
+      # Creates the dispatch for the reactor
+      #
+      # @return [Object] the dispatch.
+      def make_dispatch
+        root, app = @root, @app
+        Rack::Builder.app { map(root) { run(app) } }
+      end
+
+      # Makes sure the server supplied can run EventMachine
+      #
+      # @return [void]
+      # Raises a string error if the server does not appear to be EM compatible
+      def check_server_em_compatible
+        fail_em_incompatible unless em_compatible?
+      end
+
+      # Fails with a message about the server not being EventMachine compatible
+      def fail_em_incompatible
+        fail("Need an EM server, but #{@rack} isn't")
+      end
+
+      # Decides whether the server supplied can run EventMachine
+      #
+      # @return [Boolean] true if the server is compatible; false otherwise.
+      def em_compatible?
+        COMPATIBLE_SERVERS.include?(@rack)
+      end
+
+      COMPATIBLE_SERVERS = %w{thin hatetepe goliath}
+    end
+  end
+end
