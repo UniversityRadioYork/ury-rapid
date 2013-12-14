1501f7a7d9346bbf0cce9b65997648ac34bb37fa
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-14 22:06:22 +0000

    Add exceptions for unsupported actions.

--- exceptions.rb
+++ exceptions.rb
@@ -15,5 +15,28 @@ module Bra
 
     # Exception generated when the playout system sends an invalid message.
     InvalidPlayoutResponseError = Class.new(RuntimeError)
+
+    # Exception generated when the client requests something unsupported
+    class NotSupported < RuntimeError
+      def to_s
+        'Action not supported.'
+      end
+    end
+
+    # Exception generated when the client requests something unsupported by
+    # bra.
+    class NotSupportedByBra < NotSupported
+      def to_s
+        'Action not supported: Not supported by bra.'
+      end
+    end
+
+    # Exception generated when the client requests something that the driver
+    # cannot support
+    class NotSupportedByDriver < NotSupported
+      def to_s
+        'Action not supported: Not implemented by driver.'
+      end
+    end
   end
 end
