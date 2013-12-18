76245410427783b526d8554f926292e3f7ca9149
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-18 00:44:45 +0000

    Forgot to document WS API change.

--- common/constants.rb
+++ common/constants.rb
@@ -5,7 +5,7 @@ module Bra
       # This follows Semantic Versioning (http://semver.org).
       # When changing this, update API_CHANGE_LOG.
       MAJOR_VERSION = 0
-      MINOR_VERSION = 3
+      MINOR_VERSION = 4
       PATCH_VERSION = 0
       VERSION = [MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION].join('.')
     end
