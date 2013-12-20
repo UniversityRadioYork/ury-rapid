dcf0562f01ffcd5c9cd5a9d48a5ec9ee0b17bb5c
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-20 22:18:20 +0000

    Refactor item_spec.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,52 +2,62 @@ require 'bra/models/item'
 require 'bra/models/composite'
 
 describe Bra::Models::Item do
-  let(:item) do
-    Bra::Models::Item.new(
-      :library,
-      'Brown Girl In The Ring',
-      'playlist://0/0',
-      31415
-    )
+  subject do
+    Bra::Models::Item.new(type, name, origin, duration)
   end
+  let(:type) { :library }
+  let(:name) { 'Brown Girl In The Ring' }
+  let(:origin) { 'playlist://0/0' }
+  let(:duration) { 31415 }
+
   # Brown girl in the ring, tra la la la la
   # There's a brown girl in the ring, tra la la la la la
   # Brown girl in the ring, tra la la la la
   # She looks like a sugar in a plum, plum plum!
   describe '#flat' do
     it 'flattens the Item into a hash representation' do
-      expect(item.flat).to eq(
-        { name: 'Brown Girl In The Ring',
-          type: :library,
-          origin: 'playlist://0/0',
-          duration: 31415
+      expect(subject.flat).to eq(
+        { name: name,
+          type: type,
+          origin: origin,
+          duration: duration
         }
       )
     end
   end
   describe '#name' do
     it 'retrieves the name of the Item' do
-      expect(item.name).to eq('Brown Girl In The Ring')
+      expect(subject.name).to eq(name)
     end
   end
   describe '#type' do
     it 'retrieves the type of the Item' do
-      expect(item.type).to eq(:library)
+      expect(subject.type).to eq(type)
     end
   end
   describe '#driver_delete' do
     context 'when the Item is in a parent object' do
-      it 'removes the Item from that object, and notifies the channel' do
+      let(:parent) { Bra::Models::ListModelObject.new }
+
+      it 'removes the Item from that object' do
+        channel = double(:channel)
+        allow(channel).to receive(:push)
+        subject.register_update_channel(channel)
+
+        subject.move_to(parent, 0)
+        expect(parent.children).to eq([subject])
+        subject.driver_delete
+        expect(parent.children).to eq([])
+      end
+
+      it 'notifies the channel' do
         # Ensure the update channel is notified of the deletion
         channel = double(:channel)
-        channel.should_receive(:push).with([item, nil]).once
-        item.register_update_channel(channel)
+        channel.should_receive(:push).with([subject, nil]).once
+        subject.register_update_channel(channel)
 
-        lmo = Bra::Models::ListModelObject.new
-        item.move_to(lmo, 0)
-        expect(lmo.children).to eq([item])
-        item.driver_delete
-        expect(lmo.children).to eq([])
+        subject.move_to(parent, 0)
+        subject.driver_delete
       end
     end
   end
