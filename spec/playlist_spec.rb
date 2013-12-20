3584bd9b13691bf4ab461ae30def382218aad376
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-20 23:43:24 +0000

    Improve playlist_spec.
    There are now more tests, some of which are splits from other tests and some of
    which are entirely new.
    
    This helped catch the bug fixed in the previous commit.

--- spec/playlist_spec.rb
+++ spec/playlist_spec.rb
@@ -2,61 +2,148 @@ require 'bra/models/playlist'
 require 'bra/models/item'
 
 describe Bra::Models::Playlist do
-  let(:playlist) { Bra::Models::Playlist.new }
   let(:test1)    { Bra::Models::Item.new(:library, '1', nil, nil) }
   let(:test2)    { Bra::Models::Item.new(:library, '2', nil, nil) }
   let(:test3)    { Bra::Models::Item.new(:library, '3', nil, nil) }
 
+  let(:channel) { double(:channel) }
+  let(:handler) { double(:handler) }
+
+  before(:each) do
+    subject.register_update_channel(channel)
+    subject.register_handler(handler)
+  end
+
+  def ignore_channel
+    allow(channel).to receive(:push)
+  end
+
+  def ignore_handler
+    allow(handler).to receive(:item_handler)
+  end
+
   describe '#driver_post' do
-    context 'with a valid index and an Item' do
-      it 'inserts, registers handler and channel, and notifies the channel' do
-        handler = double(:handler)
-        channel = double(:channel)
+    context 'when the index is valid' do
+      context 'and the Item is not in the Playlist' do
+        it 'registers the playlist updates channel with the Item' do
+          ignore_handler
 
-        # As each item is POSTed, it'll ask for a handler then notify the
-        # updates channel.
-        handler.should_receive(:item_handler).with(test1).ordered
-        channel.should_receive(:push).with([test1, test1.flat]).ordered
-        handler.should_receive(:item_handler).with(test2).ordered
-        channel.should_receive(:push).with([test2, test2.flat]).ordered
-        handler.should_receive(:item_handler).with(test3).ordered
-        channel.should_receive(:push).with([test3, test3.flat]).ordered
+          channel.should_receive(:push).with([test1, test1.flat]).ordered
+          channel.should_receive(:push).with([test2, test2.flat]).ordered
+          channel.should_receive(:push).with([test3, test3.flat]).ordered
 
-        playlist.register_handler(handler)
-        playlist.register_update_channel(channel)
+          subject.driver_post(0, test1)
+          subject.driver_post(1, test2)
+          subject.driver_post(0, test3)
+        end
+        it 'registers the playlist item handler with the Item' do
+          ignore_channel
 
-        playlist.driver_post(0, test1)
-        expect(playlist.children).to eq([test1])
+          handler.should_receive(:item_handler).with(test1).ordered
+          handler.should_receive(:item_handler).with(test2).ordered
+          handler.should_receive(:item_handler).with(test3).ordered
 
-        playlist.driver_post(1, test2)
-        expect(playlist.children).to eq([test1, test2])
+          subject.driver_post(0, test1)
+          subject.driver_post(1, test2)
+          subject.driver_post(0, test3)
+        end
+        it 'adds the Item to the Playlist at the requested index' do
+          ignore_channel
+          ignore_handler
 
-        # Should insert at 0 and move the other two
-        playlist.driver_post(0, test3)
-        expect(playlist.children).to eq([test3, test1, test2])
+          subject.driver_post(0, test1)
+          expect(subject.children).to eq([test1])
+
+          subject.driver_post(1, test2)
+          expect(subject.children).to eq([test1, test2])
+
+          # Should insert at 0 and move the other two
+          subject.driver_post(0, test3)
+          expect(subject.children).to eq([test3, test1, test2])
+        end
+        it 'sets the parent of the Item to the Playlist' do
+          ignore_channel
+          ignore_handler
+
+          expect(test1.parent).to be_nil
+          subject.driver_post(0, test1)
+          expect(test1.parent).to eq(subject)
+        end
+      end
+
+      context 'and the Item is already in the Playlist' do
+        it 'moves the Item to the new Index' do
+          ignore_channel
+          ignore_handler
+
+          subject.driver_post(0, test1)
+          subject.driver_post(1, test2)
+          subject.driver_post(2, test3)
+          expect(subject.children).to eq([test1, test2, test3])
+
+          subject.driver_post(2, test1)
+          expect(subject.children).to eq([test2, test3, test1])
+
+          subject.driver_post(0, test3)
+          expect(subject.children).to eq([test3, test2, test1])
+        end
+        it 'keeps the parent as the Playlist' do
+          ignore_channel
+          ignore_handler
+
+          expect(test1.parent).to be_nil
+          subject.driver_post(0, test1)
+          expect(test1.parent).to eq(subject)
+
+          subject.driver_post(1, test2)
+          subject.driver_post(1, test1)
+          expect(test1.parent).to eq(subject)
+        end
       end
     end
   end
 
   describe '#driver_delete' do
-    context 'with Items enqueued' do
-      it 'clears the playlist and announces each item deletion' do
-        channel = double(:channel)
+    context 'when no Items are enqueued' do
+      it 'does nothing' do
+        subject.driver_delete
+      end
+    end
+    context 'when Items are enqueued' do
+      before(:each) do
+        test1.move_to(subject, 0).register_update_channel(channel)
+        test2.move_to(subject, 1).register_update_channel(channel)
+        test3.move_to(subject, 2).register_update_channel(channel)
+      end
+
+      it 'clears the Playlist' do
+        ignore_channel
+
+        expect(subject.children).to eq([test1, test2, test3])
+        subject.driver_delete
+        expect(subject.children).to be_empty
+      end
+
+      it 'sets each Item to have no parent' do
+        ignore_channel
+
+        expect(test1.parent).to eq(subject)
+        expect(test2.parent).to eq(subject)
+        expect(test3.parent).to eq(subject)
+
+        subject.driver_delete
+
+        expect(test1.parent).to be_nil
+        expect(test2.parent).to be_nil
+        expect(test3.parent).to be_nil
+      end
+
+      it 'announces each item deletion' do
         channel.should_receive(:push).with([test1, nil]).ordered
         channel.should_receive(:push).with([test2, nil]).ordered
         channel.should_receive(:push).with([test3, nil]).ordered
 
-        test1.move_to(playlist, 0).register_update_channel(channel)
-        test2.move_to(playlist, 1).register_update_channel(channel)
-        test3.move_to(playlist, 2).register_update_channel(channel)
-
-        expect(playlist.children).to eq([test1, test2, test3])
-        playlist.driver_delete
-        p(playlist.children)
-        expect(playlist.children).to be_empty
-        expect(test1.parent).to be_nil
-        expect(test2.parent).to be_nil
-        expect(test3.parent).to be_nil
+        subject.driver_delete
       end
     end
   end
