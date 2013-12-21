e39062288e531e439db6d15847960d4fc9462156
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-21 02:48:02 +0000

    Some more baps_reader_spec refactoring.

--- spec/baps_reader_spec.rb
+++ spec/baps_reader_spec.rb
@@ -8,64 +8,64 @@ describe Bra::Baps::Reader do
   end
 
   describe '#uint16' do
-    context 'when called before a full data send' do
+    context 'when called before a single, full data send' do
       let(:value) { 0 }
       it 'requests a 16-bit integer' do
-        test_number_full_data(:uint16, Bra::Baps::FormatStrings::UINT16)
+        test_full_data(:uint16, Bra::Baps::FormatStrings::UINT16)
       end
     end
     context 'when called before an incremental data send' do
       let(:value) { 32767 }
       it 'requests a 16-bit integer' do
-        test_number_split_data(:uint16, Bra::Baps::FormatStrings::UINT16)
+        test_split_data(:uint16, Bra::Baps::FormatStrings::UINT16)
       end
     end
-    context 'when called after a full data send' do
+    context 'when called after a single, full data send' do
       let(:value) { 65535 }
       it 'requests a 16-bit integer' do
-        test_number_post_data(:uint16, Bra::Baps::FormatStrings::UINT16)
+        test_post_data(:uint16, Bra::Baps::FormatStrings::UINT16)
       end
     end
   end
 
   describe '#uint32' do
-    context 'when called before a full data send' do
+    context 'when called before a single, full data send' do
       let(:value) { 0 }
       it 'requests a 32-bit integer' do
-        test_number_full_data(:uint32, Bra::Baps::FormatStrings::UINT32)
+        test_full_data(:uint32, Bra::Baps::FormatStrings::UINT32)
       end
     end
     context 'when called before an incremental data send' do
       let(:value) { 2_147_483_647 }
       it 'requests a 32-bit integer' do
-        test_number_split_data(:uint32, Bra::Baps::FormatStrings::UINT32)
+        test_split_data(:uint32, Bra::Baps::FormatStrings::UINT32)
       end
     end
-    context 'when called after a full data send' do
+    context 'when called after a single, full data send' do
       let(:value) { 4_294_967_295 }
       it 'requests a 32-bit integer' do
-        test_number_post_data(:uint32, Bra::Baps::FormatStrings::UINT32)
+        test_post_data(:uint32, Bra::Baps::FormatStrings::UINT32)
       end
     end
   end
 
   describe '#float32' do
-    context 'when called before a full data send' do
+    context 'when called before a single, full data send' do
       let(:value) { 0 }
       it 'requests a 32-bit floating-point number' do
-        test_number_full_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
+        test_full_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
       end
     end
     context 'when called before an incremental data send' do
       let(:value) { 0.125 }
       it 'requests a 32-bit floating-point number' do
-        test_number_split_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
+        test_split_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
       end
     end
-    context 'when called after a full data send' do
+    context 'when called after a single, full data send' do
       let(:value) { 512.0 }
       it 'requests a 32-bit floating-point number' do
-        test_number_post_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
+        test_post_data(:float32, Bra::Baps::FormatStrings::FLOAT32)
       end
     end
   end
@@ -73,27 +73,32 @@ describe Bra::Baps::Reader do
   describe '#string' do
     let(:value) { 'Fantasia in C Minor' }
 
-    context 'when called before data is available' do
+    context 'when called before a single, full data send' do
       it 'requests a Pascal-format string and yields it when it appears' do
-
         subject.string { |string| callback.test(string) }
         subject.add([value.bytesize].pack('N') + value)
       end
     end
+
+    context 'when called after a single, full data send' do
+      it 'requests a Pascal-format string and yields it when it appears' do
+        subject.add([value.bytesize].pack('N') + value)
+        subject.string { |string| callback.test(string) }
+      end
+    end
   end
 
-  def test_number_full_data(method, pack_format)
+  def test_full_data(method, pack_format)
     subject.send(method, &callback.method(:test))
     subject.add([value].pack(pack_format))
   end
 
-  def test_number_split_data(method, pack_format)
-
+  def test_split_data(method, pack_format)
     subject.send(method, &callback.method(:test))
     ([value].pack(pack_format)).each_char(&subject.method(:add))
   end
 
-  def test_number_post_data(method, pack_format)
+  def test_post_data(method, pack_format)
     subject.add([value].pack(pack_format))
     subject.send(method, &callback.method(:test))
   end
