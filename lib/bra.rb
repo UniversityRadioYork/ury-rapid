b22b9e317f6563e859cd0bc0c2c5bfc8d3fef360
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-24 19:56:54 +0000

    Make Bra methods module methods.

--- lib/bra.rb
+++ lib/bra.rb
@@ -4,21 +4,21 @@ require 'bra/launcher'
 require 'bra/common/constants'
 
 module Bra
-  def self.launch_from_config_file(file = nil)
+  def self.from_config_file(file = nil)
     Bra::Launcher.launch(get_config(file))
   end
 
   private
 
-  def get_config(file)
+  def self.get_config(file)
     load_config_from(file_or_default(file))
   end
 
-  def file_or_default(file)
+  def self.file_or_default(file)
     file || Bra::Common::Constants::CONFIG_FILE
   end
 
-  def load_config_from(file)
+  def self.load_config_from(file)
     YAML.load_file(file).deep_symbolize_keys!
   end
 end
