f585affde97dfd5178731e256a8a1bc91a0223f7
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-30 21:57:53 +0000

    Make tests pass.

--- lib/bra/model/component_creator.rb
+++ lib/bra/model/component_creator.rb
@@ -1,9 +1,18 @@
 require 'bra/common/types'
+require 'bra/model'
 
 module Bra
   module Model
     class ComponentCreator
-      def create
+      def create(type, value)
+        case type
+        when :load_state
+          fail unless Bra::Common::Types::LOAD_STATES.include? value
+          Bra::Model::Constant.new(value, :load_state)
+        when :play_state
+          fail unless Bra::Common::Types::PLAY_STATES.include? value
+          Bra::Model::Constant.new(value, :state)
+        end
       end
     end
   end
