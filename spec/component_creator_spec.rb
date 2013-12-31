7c2e53fb33c4ad4966d5abac551d1bd48dcf98b0
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-31 14:58:32 +0000

    Expect the registrar to be called.

--- spec/component_creator_spec.rb
+++ spec/component_creator_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'bra/model/component_creator'
+require 'bra/model'
 
 shared_examples 'a valid symbol constant' do |type, value|
   it "returns an object whose flat representation is :#{value}" do
@@ -8,8 +9,19 @@ shared_examples 'a valid symbol constant' do |type, value|
   end
 end
 
+shared_examples 'a successful factory method' do |type, value|
+  it 'sends the registrar #register with an object to register' do
+    expect(registrar).to receive(:register).once do |arg|
+      expect(arg).to be_a(Bra::Model::ModelObject)
+    end
+    subject.send(type, value)
+  end
+end
+
 shared_examples 'a symbol constant' do |type, valid_list|
   valid_list.each do |valid|
+    it_behaves_like 'a successful factory method', type, valid
+
     context "when the argument is :#{valid}" do
       it_behaves_like 'a valid symbol constant', type, valid
     end
@@ -30,6 +42,7 @@ end
 describe Bra::Model::ComponentCreator do
   subject { Bra::Model::ComponentCreator.new(registrar) }
   let(:registrar) { double(:registrar) }
+  before(:each) { allow(registrar).to receive(:register) }
 
   describe '#load_state' do
     it_behaves_like(
