f08dab82fb0cd703b78003869f5d3db736141cc9
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-31 01:19:23 +0000

    Use validators to pass this test.

--- lib/bra/model/component_creator.rb
+++ lib/bra/model/component_creator.rb
@@ -4,14 +4,14 @@ require 'bra/model'
 module Bra
   module Model
     class ComponentCreator
+      include Bra::Common::Types::Validators
+
       def create(type, value)
         case type
         when :load_state
-          fail unless Bra::Common::Types::LOAD_STATES.include? value
-          Bra::Model::Constant.new(value, :load_state)
+          Bra::Model::Constant.new(validate_load_state(value), :load_state)
         when :play_state
-          fail unless Bra::Common::Types::PLAY_STATES.include? value
-          Bra::Model::Constant.new(value, :state)
+          Bra::Model::Constant.new(validate_play_state(value), :state)
         end
       end
     end
