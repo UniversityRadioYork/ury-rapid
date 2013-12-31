d94c172265421a71667f0efefd1f6a3da64b95a1
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-31 01:47:11 +0000

    Replace #create with direct methods in spec.

--- spec/component_creator_spec.rb
+++ spec/component_creator_spec.rb
@@ -3,7 +3,7 @@ require 'bra/model/component_creator'
 
 shared_examples 'a valid symbol constant' do |type, value|
   it "returns an object whose flat representation is :#{value}" do
-    created = subject.create(type, value)
+    created = subject.send(type, value)
     expect(created.flat).to eq(value.to_sym)
   end
 end
@@ -19,31 +19,29 @@ shared_examples 'a symbol constant' do |type, valid_list|
   end
 
   context 'when the argument is invalid' do
-    specify { expect { subject.create(type, :xyzzy) }.to raise_error }
+    specify { expect { subject.send(type, :xyzzy) }.to raise_error }
   end
 
   context 'when the argument is nil' do
-    specify { expect { subject.create(type, nil) }.to raise_error }
+    specify { expect { subject.send(type, nil) }.to raise_error }
   end
 end
 
 describe Bra::Model::ComponentCreator do
-  describe '#create' do
-    context 'when the type is :load_state' do
-      it_behaves_like(
-        'a symbol constant',
-        :load_state,
-        Bra::Common::Types::LOAD_STATES
-      )
-    end
+  describe '#load_state' do
+    it_behaves_like(
+      'a symbol constant',
+      :load_state,
+      Bra::Common::Types::LOAD_STATES
+    )
+  end
 
-    context 'when the type is :play_state' do
-      it_behaves_like(
-        'a symbol constant',
-        :play_state,
-        Bra::Common::Types::PLAY_STATES
-      )
-    end
+  describe '#play_state' do
+    it_behaves_like(
+      'a symbol constant',
+      :play_state,
+      Bra::Common::Types::PLAY_STATES
+    )
   end
 end
 
