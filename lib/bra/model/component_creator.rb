eb5ef7e9ca306335a417913900ce98b044964645
mattbw <matt.windsor@ury.york.ac.uk>
2013-12-31 15:02:13 +0000

    Make it pass.

--- lib/bra/model/component_creator.rb
+++ lib/bra/model/component_creator.rb
@@ -4,18 +4,33 @@ require 'bra/model'
 module Bra
   module Model
     class ComponentCreator
+      extend Forwardable
+
       include Bra::Common::Types::Validators
 
       def initialize(registrar)
+        @registrar = registrar
       end
 
       def load_state(value)
-        Bra::Model::Constant.new(validate_load_state(value), :load_state)
+        validate_then_constant(:validate_load_state, value, :load_state)
       end
 
       def play_state(value)
-        Bra::Model::Constant.new(validate_play_state(value), :state)
+        validate_then_constant(:validate_play_state, value, :state)
       end
+
+      private
+
+      def validate_then_constant(validator, raw_value, handler_target)
+        constant(send(validator, raw_value), handler_target)
+      end
+
+      def constant(value, handler_target)
+        Bra::Model::Constant.new(value, handler_target).tap(&method(:register))
+      end
+
+      def_delegator :@registrar, :register
     end
   end
 end
