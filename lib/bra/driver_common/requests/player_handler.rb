5237035306f5945089dc168155d0b61e293559bb
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 23:03:31 +0000

    Implement PlaylistHandler and update PlayerHandler.

--- lib/bra/driver_common/requests/player_handler.rb
+++ lib/bra/driver_common/requests/player_handler.rb
@@ -5,37 +5,53 @@ require 'bra/driver_common/requests/playlist_reference_parser'
 module Bra
   module DriverCommon
     module Requests
-      # Extension of request handler to deal with the common Player APIs
+      # Base class for handlers that handle requests on playyers
       #
-      # This deals with the various protocols for POSTing player items, so that
-      # drivers can override the protocol methods they implement
+      # This provides boilerplate code for working with the standard bra player
+      # API.
       class PlayerHandler < UrlHashHandler
         include PlaylistReferenceParser
         alias_method :caller_id, :local_playlist_id
 
+        # The only objects this handler takes responsibility for POSTing to the
+        # playout server are items, which always come in on ID :item.
+        # Every other POST goes to the ID'd child as a PUT.
         use_payload_processor_for :post, :item
         post_by_putting_to_child_for :volume, :state, :load_state
         post_by_putting_to_child_for :position, :cue, :intro
 
+        # In the current version of the bra API, the standard methods for
+        # POSTING an item into a player are:
+
+        # playlist
+        #   URL: 'playlist://[src-playlist/]src-index
+        #   Hash: {type: :playlist, [playlist: src-playlist,] index: src-index}
+        #
+        #   Loads an item that is already at the given playlist reference.
+        #
+        #   If no source playlist is given, the playlist with the same ID as
+        #   this player is substituted.  This can be useful in playout systems
+        #   with linked players and playlists.
+        #
+        #   Drivers MAY choose not to implement this command.
+        #
+        #   Drivers may implement this by overriding #item_from_local_playlist
+        #   and #item_from_foreign_playlist.
         # Supported URL protocols in this version of the bra API.
         url_type :playlist do |url|
           item_from_playlist(*parse_playlist_reference_url(url))
         end
-
-        # Supported hash types in this version of the bra API.
         hash_type :playlist do |hash|
           item_from_playlist(*parse_playlist_reference_hash(hash))
         end
 
-        # These are the overridable functions a concrete PlayerPoster can fill
+        # These are the overridable functions a concrete PlayerHandler can fill
         # in.  They are defined in this class as raising a NotSupportedByDriver
         # exception.
         TO_OVERRIDE = [
           :item_from_local_playlist,  # index
-          :item_from_other_playlist   # playlist_id, index
+          :item_from_foreign_playlist # playlist_id, index
         ]
-
-        # Set up stubs for each method we expect the driver to override.
         TO_OVERRIDE.each do |method_symbol|
           define_method(method_symbol) do |*args|
             fail(Bra::Common::Exceptions::NotSupportedByDriver)
@@ -45,15 +61,9 @@ module Bra
         protected
 
         def item_from_playlist(playlist, index)
-          local = is_local_playlist?(playlist)
-          item_from_local_playlist(index)           if local
-          item_from_other_playlist(playlist, index) unless local
-        end
-
-        # @return [Boolean] True if the playlist ID is the same as the player
-        #   ID.
-        def is_local_playlist?(playlist)
-          caller_id == playlist || caller_id.to_s == playlist
+          local = local_playlist?(playlist)
+          item_from_local_playlist(index)                 if local
+          item_from_foreign_playlist(playlist, index) unless local
         end
       end
     end
--- lib/bra/driver_common/requests/playlist_handler.rb
+++ lib/bra/driver_common/requests/playlist_handler.rb
@@ -8,8 +8,48 @@ module Bra
       # This provides boilerplate code for working with the standard bra
       # playlist API.
       class PlaylistHandler < UrlHashHandler
+        # All objects POSTed to the playlist will be items, so we process their
+        # payloads to find out what sort of item load they represent.
         use_payload_processor_for :post
-        put_by_posting_to_parent
+
+        # In the current version of the bra API, the standard methods for
+        # POSTing an item into a playlist are:
+
+        # move
+        #   URL: 'move://[old-playlist/]old-index
+        #   Hash: {type: :move, [playlist: old-playlist,] index: old-index}
+        #   
+        #   Moves the item from the given playlist reference to the playlist
+        #   and index it is being POSTed to.
+        #
+        #   Drivers MAY choose not to implement non-local playlist moves, but
+        #   SHOULD implement some form of move.
+        #
+        #   Drivers may implement this by overriding #move_local and
+        #   #move_foreign.
+        url_type  :move { |url|  move(*parse_playlist_reference_url(url)) }
+        hash_type :move { |hash| move(*parse_playlist_reference_hash(hash)) }
+
+        # These are the overridable functions a concrete PlaylistHandler can
+        # fill in.  They are defined in this class as raising a
+        # NotSupportedByDriver exception.
+        TO_OVERRIDE = [
+          :move_from_local_playlist,  # index
+          :move_from_foreign_playlist # playlist_id, index
+        ]
+        TO_OVERRIDE.each do |method_symbol|
+          define_method(method_symbol) do |*args|
+            fail(Bra::Common::Exceptions::NotSupportedByDriver)
+          end
+        end
+
+        protected
+
+        def move(playlist, index)
+          local = local_playlist?(playlist)
+          move_from_local_playlist(index)                 if local
+          move_from_foreign_playlist(playlist, index) unless local
+        end
       end
     end
   end
