c282851ce2bc4441101b510dbf627203594881bd
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 21:41:35 +0000

    Outline desired methods.

--- spec/playlist_reference_parser_spec.rb
+++ spec/playlist_reference_parser_spec.rb
@@ -5,4 +5,12 @@ class MockPrp < Bra::DriverCommon::Requests::PlaylistReferenceParser
 end
 
 describe MockPrp do
+  describe '#local_playlist?' do
+  end
+
+  describe '#parse_playlist_reference_url' do
+  end
+
+  describe '#parse_playlist_reference_hash' do
+  end
 end
