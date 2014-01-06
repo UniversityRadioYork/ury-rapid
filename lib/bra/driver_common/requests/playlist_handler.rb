07b1a4d6e849562ae10ee21e9d3e69af53cadbf5
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 21:16:53 +0000

    Documentation string

--- lib/bra/driver_common/requests/playlist_handler.rb
+++ lib/bra/driver_common/requests/playlist_handler.rb
@@ -4,6 +4,10 @@ module Bra
   module DriverCommon
     module Requests
       module Handlers
+        # Base class for handlers that handle requests on playlists
+        # 
+        # This provides boilerplate code for working with the standard bra
+        # playlist API.
         class PlaylistHandler < UrlHashHandler
         end
       end
