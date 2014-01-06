285c956b8c3e6cf04cda418c013ab42545271389
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 21:43:17 +0000

    Specify #local_playlist?

--- spec/playlist_reference_parser_spec.rb
+++ spec/playlist_reference_parser_spec.rb
@@ -2,10 +2,20 @@ require 'bra/driver_common/requests/playlist_reference_parser'
 
 # Mock implementation of PlaylistReferenceParser.
 class MockPrp < Bra::DriverCommon::Requests::PlaylistReferenceParser 
+  def local_playlist
+    :local_id
+  end
 end
 
 describe MockPrp do
   describe '#local_playlist?' do
+    context 'when the given playlist equals #local playlist' do
+      specify { expect(subject.local_playlist(:local_id)).to be_true }
+    end
+
+    context 'when the given playlist does not equal #local playlist' do
+      specify { expect(subject.local_playlist(:foreign_id)).to be_false }
+    end
   end
 
   describe '#parse_playlist_reference_url' do
