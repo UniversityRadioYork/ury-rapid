baed658b8a5d88628f2bc29328b7a8b5f8195585
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 22:47:21 +0000

    Use reference parser in PlayerHandler.

--- lib/bra/driver_common/requests/player_handler.rb
+++ lib/bra/driver_common/requests/player_handler.rb
@@ -1,5 +1,6 @@
 require 'bra/driver_common/requests/handler'
 require 'bra/driver_common/requests/url_hash_handler'
+require 'bra/driver_common/requests/playlist_reference_parser'
 
 module Bra
   module DriverCommon
@@ -9,18 +10,21 @@ module Bra
       # This deals with the various protocols for POSTing player items, so that
       # drivers can override the protocol methods they implement
       class PlayerHandler < UrlHashHandler
+        include PlaylistReferenceParser
+        alias_method :caller_id, :local_playlist_id
+
         use_payload_processor_for :post, :item
         post_by_putting_to_child_for :volume, :state, :load_state
         post_by_putting_to_child_for :position, :cue, :intro
 
         # Supported URL protocols in this version of the bra API.
         url_type :playlist do |url|
-          item_from_playlist(*split_playlist_url(url))
+          item_from_playlist(*parse_playlist_reference_url(url))
         end
 
         # Supported hash types in this version of the bra API.
         hash_type :playlist do |hash|
-          item_from_playlist(*get_location_from_hash(hash))
+          item_from_playlist(*parse_playlist_reference_hash(hash))
         end
 
         # These are the overridable functions a concrete PlayerPoster can fill
@@ -39,20 +43,6 @@ module Bra
         end
 
         protected
-
-        def split_playlist_url(url)
-          split = url.split('/', 2)
-          playlist, index = caller_id, split.first if split.size == 1
-          playlist, index = split                  if split.size == 2
-          fail('Bad playlist URL.')                if split.size > 2
-          [playlist, index.to_i]
-        end
-
-        def get_location_from_hash(hash)
-          playlist = hash[:playlist] || caller_id
-          index    = hash[:index]    || 0
-          [playlist, index.to_i]
-        end
 
         def item_from_playlist(playlist, index)
           local = is_local_playlist?(playlist)
