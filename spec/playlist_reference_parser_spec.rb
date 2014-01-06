0215a312135e32aca2eb2a2a2fea364b04706358
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 23:25:38 +0000

    Specify likewise for the hash parser.

--- spec/playlist_reference_parser_spec.rb
+++ spec/playlist_reference_parser_spec.rb
@@ -21,8 +21,8 @@ describe MockPrp do
   end
 
   describe '#parse_playlist_reference_url' do
-    context 'when given a String representing a natural' do
-      it 'returns an Array containing #local_playlist_id and the natural' do
+    context 'when given a String representing an Integer' do
+      it 'returns an Array containing #local_playlist_id and the Integer' do
         [0, 1, 10, 100, 23456].each do |example|
           expect(
             subject.parse_playlist_reference_url(example.to_s)
@@ -31,9 +31,9 @@ describe MockPrp do
       end
     end
 
-    context 'when given a String with a slash followed by a natural' do
+    context 'when given a String with a slash followed by an Integer' do
       context 'and the prefix represents an Integer' do
-        it 'returns an Array of the prefix as an Integer and the natural' do
+        it 'returns an Array of the prefix as an Integer and the Integer' do
           [0, 1, 10, 100, 23456].each do |number|
             [2, 4, 6, 8, 999].each do |playlist|
               expect(
@@ -44,7 +44,7 @@ describe MockPrp do
         end
       end
       context 'and the prefix does not represent an Integer' do
-        it 'returns an Array of the prefix as a Symbol and the natural' do
+        it 'returns an Array of the prefix as a Symbol and the Integer' do
           [0, 1, 10, 100, 23456].each do |number|
             %w{flibble dibble purple doggie doo}.each do |playlist|
               expect(
@@ -56,7 +56,7 @@ describe MockPrp do
       end
     end
 
-    context 'when given a String with a slash followed by a non-natural' do
+    context 'when given a String with a slash followed by a non-Integer' do
       specify do
         expect { subject.parse_playlist_reference_url("squir/tle") }.to(
           raise_error
@@ -81,7 +81,7 @@ describe MockPrp do
         let(:index) { 22 }
 
         it 'returns an Array containing a playlist ID and the Integer' do
-          expect(test.call).to eq([playlist.to_sym, 22])
+          expect(test.call).to eq([expected_playlist, 22])
         end
       end
 
@@ -89,7 +89,7 @@ describe MockPrp do
         let(:index) { '44' }
 
         it 'returns an Array containing a playlist ID and the Integer' do
-          expect(test.call).to eq([playlist.to_sym, 44])
+          expect(test.call).to eq([expected_playlist, 44])
         end
       end
 
@@ -107,8 +107,8 @@ describe MockPrp do
     end
 
     context 'when given a Hash with no :playlist key' do
-      let(:hash)     { { index: index } }
-      let(:playlist) { subject.local_playlist_id }
+      let(:hash)              { { index: index } }
+      let(:expected_playlist) { subject.local_playlist_id }
 
       it_behaves_like 'a normal call with an index'
 
@@ -120,9 +120,36 @@ describe MockPrp do
     end
 
     context 'when given a Hash with a :playlist key' do
-      let(:hash) { { playlist: playlist, index: index } }
+      let(:hash)              { { playlist: playlist, index: index } }
+      let(:expected_playlist) { playlist.to_sym }
 
-      context 'and :playlist is a string' do
+      context 'and :playlist is an Integer' do
+        let(:playlist) { 20 }
+        let(:expected_playlist) { playlist }
+
+        it_behaves_like 'a normal call with an index'
+
+        context 'and :index is not given' do
+          let(:hash) { { playlist: playlist } }
+
+          specify { expect { test.call }.to(raise_error) }
+        end
+      end
+
+      context 'and :playlist is a string representing an Integer' do
+        let(:playlist) { '20' }
+        let(:expected_playlist) { Integer(playlist) }
+
+        it_behaves_like 'a normal call with an index'
+
+        context 'and :index is not given' do
+          let(:hash) { { playlist: playlist } }
+
+          specify { expect { test.call }.to(raise_error) }
+        end
+      end
+
+      context 'and :playlist is a string not representing an Integer' do
         let(:playlist) { 'a_playlist' }
 
         it_behaves_like 'a normal call with an index'
