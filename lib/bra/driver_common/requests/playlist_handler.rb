6003ebd08e0402586c9b6311d00d46df49c0a972
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 21:33:25 +0000

    Move PlaylistHandler up a module level.

--- lib/bra/driver_common/requests/playlist_handler.rb
+++ lib/bra/driver_common/requests/playlist_handler.rb
@@ -3,15 +3,13 @@ require 'bra/driver_common/requests/url_hash_handler'
 module Bra
   module DriverCommon
     module Requests
-      module Handlers
-        # Base class for handlers that handle requests on playlists
-        # 
-        # This provides boilerplate code for working with the standard bra
-        # playlist API.
-        class PlaylistHandler < UrlHashHandler
-          use_payload_processor_for :post
-          put_by_posting_to_parent
-        end
+      # Base class for handlers that handle requests on playlists
+      #
+      # This provides boilerplate code for working with the standard bra
+      # playlist API.
+      class PlaylistHandler < UrlHashHandler
+        use_payload_processor_for :post
+        put_by_posting_to_parent
       end
     end
   end
