3e6ed36c15c5e109bd479304d00e904fe2e71c30
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-06 21:38:52 +0000

    Make stub spec for PlaylistReferenceParser.

---
+++ spec/playlist_reference_parser_spec.rb
@@ -0,0 +1,8 @@
+require 'bra/driver_common/requests/playlist_reference_parser'
+
+# Mock implementation of PlaylistReferenceParser.
+class MockPrp < Bra::DriverCommon::Requests::PlaylistReferenceParser 
+end
+
+describe MockPrp do
+end
