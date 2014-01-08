a7d714232cb084d538bc3601888a3233c6318e9b
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-08 22:13:18 +0000

    Make #log public, fix URL typo.

--- lib/bra/model/view.rb
+++ lib/bra/model/view.rb
@@ -20,6 +20,21 @@ module Bra
       def_delegator :@root, :register_for_updates
       def_delegator :@root, :deregister_from_updates
 
+      # Logs an error message in the bra log
+      # 
+      # @api  public
+      # @example  Log an error.
+      #   view.log(:error, 'The system is down!')
+      #
+      # @param severity [Symbol]  The severity level of the log message.  This
+      #   must be one of :debug, :info, :warn, :error or :fatal.
+      # @param message [String]  The log message itself.
+      #
+      # @return [void]
+      def log(severity, message)
+        find('log') { |log| log.driver_post(severity, message) }
+      end
+
       protected
 
       # Finds a model object given its URL
@@ -28,20 +43,7 @@ module Bra
       #
       # @return [ModelObject]  The found model object.
       def find(url, &block)
-        Compo::UrlFinder.find(@root, url, &block)
-      end
-
-      # Logs an error message in the bra log
-      # 
-      # @api private
-      #
-      # @param severity [Symbol]  The severity level of the log message.  This
-      #   must be one of :debug, :info, :warn, :error or :fatal.
-      # @param message [String]  The log message itself.
-      #
-      # @return [void]
-      def log(severity, message)
-        find('/log') { |log| log.driver_post(severity, message) }
+        Finder.find(@root, url, &block)
       end
     end
   end
