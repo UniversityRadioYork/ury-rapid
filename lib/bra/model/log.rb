d55e9eb27428b1e56aa0e27d6c77e0903a5f3eb5
mattbw <matt.windsor@ury.york.ac.uk>
2014-01-09 02:07:36 +0000

    No point logging the Log init - it breaks rspecs

--- lib/bra/model/log.rb
+++ lib/bra/model/log.rb
@@ -4,18 +4,18 @@ require 'bra/model/model_object'
 module Bra
   module Model
     # An object that represents bra's error/debugging log
-    # 
+    #
     # This is part of the model to allow easy access to the logger in areas
     # such as the server and driver, without needing to pass it in
     # explicitly.  It also allows the log to be treated like any other bit of
     # data bra holds.
-    # 
+    #
     # In future, the log may be readable using the bra API.
     class Log < Compo::Leaf
       include ModelObject
 
       # Initialises the Log
-      # 
+      #
       # @api  public
       # @example  Initialising a log with an instance of a Ruby Logger
       #   log = Log.new(Logger.new(STDOUT))
@@ -24,10 +24,7 @@ module Bra
       #   Logger's API.
       def initialize(logger)
         super()
-
         @logger = logger
-
-        logger.info('Log created.')
       end
 
       # POSTs an entry to this Log
