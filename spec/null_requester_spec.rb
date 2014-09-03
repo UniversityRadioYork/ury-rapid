422f0cfd68514f52f2d217591b78da8b009d836e
Matt Windsor <mattwindsor@btinternet.com>
2014-09-03 20:29:26 +0000

    Add null requester.
    The null requester responds to #add_handlers, but ignores it.

---
+++ spec/null_requester_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+require 'ury-rapid/service_common/requests/null_requester'
+
+describe Rapid::ServiceCommon::Requests::NullRequester do
+  describe '#add_handlers' do
+    it 'does not change the model structure' do
+      structure = double(:structure)
+      expect { build(:null_requester).add_handlers(structure) }
+                                     .not_to change { structure }
+    end
+  end
+end
