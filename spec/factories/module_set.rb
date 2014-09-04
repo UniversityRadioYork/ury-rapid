1ff3e3f1ec465fbea40fa8dca68867d4f0c05c35
Matt Windsor <mattwindsor@btinternet.com>
2014-09-04 00:24:54 +0000

    Refactor ModuleSet spec.
    This now uses FactoryGirl, and doesn't use let or subject.  Also fixed
    some typos.  Woop woop!

---
+++ spec/factories/module_set.rb
@@ -0,0 +1,38 @@
+require 'ury-rapid/modules/set'
+
+class DummyModule
+  def new(*_args)
+  end
+
+  def run
+  end
+end
+
+class DummyModelBuilder
+  def new(*_args)
+  end
+
+  def build(_name, _mod)
+  end
+end
+
+FactoryGirl.define do
+  factory :module_set, class: Rapid::Modules::Set do
+    model_builder DummyModelBuilder.new
+
+    factory(:empty_module_set) {}
+
+    factory :non_empty_module_set do
+      ignore do
+        modules %i(module1 module3 module5 module7)
+        enabled []
+        module_class DummyModule
+      end
+
+      after :build do |set, ev|
+        ev.modules.each { |m| set.configure(m, ev.module_class) {} }
+        ev.enabled.each { |m| set.enable(m) }
+      end
+    end
+  end
+end
--- spec/module_set_spec.rb
+++ spec/module_set_spec.rb
@@ -3,118 +3,147 @@ require 'ury-rapid/modules/set'
 describe Rapid::Modules::Set do
   subject { Rapid::Modules::Set.new }
 
-  class DummyModule
-    def run
-    end
-  end
-
-  before(:each) do
-    modules.each { |m| subject.configure(m, DummyModule) {} }
-  end
-
-  let(:modules) { %i(module1 module3 module5 module7) }
-
   describe '#enable_all' do
     context 'when no modules are configured' do
-      let(:modules) { [] }
-
       # Enabling 0 modules should be a no-op.
-      specify { expect { subject.enable_all.to_not raise_error } }
+      specify do
+        set = build(:empty_module_set)
+
+        expect { set.enable_all }.not_to raise_error
+      end
 
       it 'adds no modules to #enabled' do
-        expect { subject.enable_all }.to_not change { subject.enabled }
-        expect(subject.enabled).to be_empty
+        set = build(:empty_module_set)
+
+        expect { set.enable_all }.not_to change { set.enabled }
+        expect(set.enabled).to be_empty
       end
 
       it 'removes no modules from #disabled' do
-        expect { subject.enable_all }.to_not change { subject.disabled }
+        set = build(:empty_module_set)
+
+        expect { set.enable_all }.not_to change { set.disabled }
       end
     end
 
     context 'when some modules are configured' do
-      specify { expect { subject.enable_all.to_not raise_error } }
-
       it 'adds all modules to #enabled' do
-        expect { subject.enable_all }.to change { subject.enabled }
-                                     .from([])
-        expect(subject.enabled).to contain_exactly(*modules)
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect { set.enable_all }.to change { set.enabled }.from([])
+        expect(set.enabled).to contain_exactly(:foo, :bar)
       end
 
       it 'empties #disabled' do
-        expect(subject.disabled).to contain_exactly(*modules)
-        expect { subject.enable_all }.to change { subject.disabled }
-                                     .to([])
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect(set.disabled).to contain_exactly(:foo, :bar)
+        expect { set.enable_all }.to change { set.disabled }.to([])
       end
     end
   end
 
   describe '#enable' do
     context 'when the module is not configured' do
-      specify { expect { subject.enable(:module2).to raise_error } }
+      specify do
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect { set.enable(:baz) }.to raise_error
+      end
     end
 
     context 'when the module is configured' do
-      specify { expect { subject.enable(:module1).to_not raise_error } }
+      specify do
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect { set.enable(:foo) }.not_to raise_error
+        expect { set.enable(:bar) }.not_to raise_error
+      end
 
       it 'adds the module to #enabled' do
-        expect { subject.enable(:module1) }.to change { subject.enabled }
-                                           .from([])
-        expect(subject.enabled).to include(:module1)
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect { set.enable(:foo) }.to change { set.enabled }.from([])
+        expect(set.enabled).to include(:foo)
       end
 
       it 'removes the module from #disabled' do
-        expect(subject.disabled).to include(:module1)
-        expect { subject.enable(:module1) }.to change { subject.disabled }
-        expect(subject.disabled).to_not include(:module1)
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect(set.disabled).to include(:foo)
+        expect { set.enable(:foo) }.to change { set.disabled }
+        expect(set.disabled).not_to include(:foo)
       end
     end
 
     context 'when the module has already been enabled' do
-      before(:each) { subject.enable(:module1) }
-
       specify do
-        expect { subject.enable(:module1) }.to_not change { subject.enabled }
+        set = build(:non_empty_module_set,
+                    modules: %i(foo bar),
+                    enabled: %i(foo))
+
+        expect { set.enable(:foo) }.not_to change { set.enabled }.from([:foo])
       end
 
       specify do
-        expect { subject.enable(:module1) }.to_not change { subject.disabled }
+        set = build(:non_empty_module_set,
+                    modules: %i(foo bar),
+                    enabled: %i(foo))
+
+        expect { set.enable(:foo) }.not_to change { set.disabled }.from([:bar])
       end
     end
   end
 
   describe '#start' do
-    let(:mb) { double(:model_builder) }
-
-    before(:each) do
-      allow(mb).to receive(:build)
-      subject.model_builder = mb
-    end
-
     context 'when the module is not configured' do
-      specify { expect { subject.start(:module2).to raise_error } }
+      specify do
+        set = build(:non_empty_module_set, modules: %i(foo bar))
+
+        expect { set.start(:baz) }.to raise_error
+      end
     end
 
     context 'when the module is configured' do
-      before(:each) do
-        dmnew = DummyModule.method(:new)
-        allow(DummyModule).to receive(:new) do |*args|
-          dmnew.call(*args)
+      class FakeModule
+        def new(*_args)
+        end
+
+        def run
         end
       end
 
-      specify { expect { subject.start(:module1).to_not raise_error } }
+      it 'proceeds without error' do
+        set = build(:non_empty_module_set,
+                    modules: %i(foo bar),
+                    enabled: %i(foo))
+
+        expect { set.start(:foo) }.not_to raise_error
+        expect { set.start(:bar) }.not_to raise_error
+      end
 
       it 'instantiates the DummyModule' do
-        subject.start(:module1)
-        expect(DummyModule).to have_received(:new).once
+        md = double(:module)
+        allow(md).to receive(:run)
+
+        allow(FakeModule).to receive(:new).and_return(md)
+
+        build(:non_empty_module_set,
+              modules: [:foo],
+              module_class: FakeModule).start(:foo)
+        expect(FakeModule).to have_received(:new).once
       end
 
       it 'calls #build on the model builder with a module name and instance' do
-        subject.start(:module1)
-        expect(mb).to have_received(:build).once.with(
-          :module1,
-          a_kind_of(DummyModule)
-        )
+        mb = double(:model_builder)
+        allow(mb).to receive(:build)
+
+        build(:non_empty_module_set,
+              modules: [:foo],
+              module_class: FakeModule,
+              model_builder: mb).start(:foo)
+        expect(mb).to have_received(:build)
+                  .once.with(:foo, a_kind_of(FakeModule))
       end
     end
   end
