fa9f9472c07fac4a65eba2ffafbd44a0f44fbbf4
Matt Windsor <matt.windsor@ury.org.uk>
2014-11-23 13:53:33 +0000

    Add CodeTable test.

--- minitest/code_table_test.rb
+++ minitest/code_table_test.rb
@@ -1,23 +1,26 @@
-require 'test/unit'
+require 'minitest'
 
-class CodeTableTest < Test::Unit::TestCase
+require 'ury_rapid/services/code_table'
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
+# A dummy code table, for the CodeTableTest test
+module DummyCodeTable
+  extend Rapid::Services::CodeTable
+
+  module Foo
+    FOO = 0xF00
+    BAR = 0xBAA
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
+  module Foobar
+    OOPS = 0xDEADBEEF
   end
+end
 
-  # Fake test
-  def test_fail
-
-    fail('Not implemented')
+# Tests for the CodeTable module mixin
+class CodeTableTest < Minitest::Test
+  # Tests whether CodeTable#code_symbol works for a valid code
+  def test_code_symbol_valid
+    assert_equal('DummyCodeTable::Foobar::OOPS',
+                 DummyCodeTable.code_symbol(DummyCodeTable::Foobar::OOPS))
   end
 end
